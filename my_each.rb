f07d9efa5863019cd4ef3130af26ee374599495f
Angie Kassab <anasab12@gmail.com>
2018-07-19 17:09:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+ puts "This is only a test!"
+ 
+ array.each do |name|
+  yield
+    puts name
+  while array.length < 5
+    puts "My name is #{name}, and I'm next"
+  array.length == 5
+    puts "This is the end of the line!"
+  end 
+end
+
+end 
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
