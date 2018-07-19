c23f353a8c2b203840d9e736216c1d9ef4beda54
Angie Kassab <anasab12@gmail.com>
2018-07-19 17:16:25 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,14 +3,13 @@ def my_each(array) # put argument(s) here
  
  yield
     puts num
-    i + 0
-  while array.length < 5
+ i + 0
+while array.length < 5
     puts "My num is #{num}, and I'm next"
-    i += 1
+ i += 1
   end 
 end
 
-end 
 
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
